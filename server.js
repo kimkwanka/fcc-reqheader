530dc31a3d745383342a4d9e89814da457d4665b
Kim Kwanka <kimkwanka@gmail.com>
2017-02-16 12:02:34 +0100

    Refactored code to be ESLint (AirBNB Style) compliant and added .eslintrc to .gitignore

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
 const stylus = require('stylus');
 
 const app = express();
 
-//Use process.env.PORT if set for Heroku, AWS, etc.
+// Use process.env.PORT if set for Heroku, AWS, etc.
 const port = process.env.PORT || 8080;
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
-//Enable Stylus preprocessor as middleware
+// Enable Stylus preprocessor as middleware
 app.use(stylus.middleware({
   src: path.join(__dirname, '/res'),
   dest: path.join(__dirname, '/public'),
-  compile: ((str, filepath) => {
-    return stylus(str)
+  compile: (str, filepath) => (
+    stylus(str)
       .set('filename', filepath)
-      .set('compress', true);
-  })
+      .set('compress', true)
+    ),
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res, next) {
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'ReqHeader Parser',
-    url: 'https://fcc-reqheader.herokuapp.com'
+    url: 'https://fcc-reqheader.herokuapp.com',
   });
 });
 
-app.get('/api', function (req, res, next) {
-  
-  let ip = req.headers['x-forwarded-for'] || req.ip;
-  let lang = req.acceptsLanguages()[0];
-  let soft = req.headers['user-agent'].match(/\(([^)]+)\)/)[1];
+app.get('/api', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.ip;
+  const lang = req.acceptsLanguages()[0];
+  const soft = req.headers['user-agent'].match(/\(([^)]+)\)/)[1];
 
   res.json({
     ipaddress: ip,
     language: lang,
-    software: soft
+    software: soft,
   });
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.render('404', {});
 });
 
 app.listen(port);
 
-//export functions for testing in server-test.js
+// export functions for testing in server-test.js
 module.exports = {};
